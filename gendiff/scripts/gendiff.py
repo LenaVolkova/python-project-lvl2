b96b448a4ec674842354bf7cd8c9fefea7f556f3
Elena Volkova <lenochka.volkova@gmail.com>
2021-10-10 18:27:48 +0300

    step 5

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,18 +3,16 @@
 
 import sys
 import argparse
-import yaml
-from gendiff.scripts.process_json import process_json 
+from gendiff.scripts.process_json import process_json
 from gendiff.scripts.process_yaml import process_yaml
 from operator import itemgetter
-
 
 
 def generate_diff(filepath1, filepath2):
     data1 = {}
     data2 = {}
-    data_diff = []
-    result_diff = ""
+    data_for_diff = []
+    diff = ""
 
     if filepath1[-5:] == '.json':
         content1 = process_json(filepath1)
@@ -23,7 +21,7 @@ def generate_diff(filepath1, filepath2):
     if filepath1[-5:] == '.yaml' or filepath1[-4:] == '.yml':
         content1 = process_yaml(filepath1)
         content2 = process_yaml(filepath2)
-        
+
     for key1, value1 in content1.items():
         if str(value1) == "True":
             value1 = "true"
@@ -36,27 +34,26 @@ def generate_diff(filepath1, filepath2):
         if str(value2) == "False":
             value2 = "false"
         data2[key2] = value2
-    
 
     for key in data1:
         if key in data2 and data1[key] == data2[key]:
-            data_diff.append((' ', key, data1[key]))
+            data_for_diff.append((' ', key, data1[key]))
         elif key in data2 and data1[key] != data2[key]:
-            data_diff.append(('-', key, data1[key]))
-            data_diff.append(('+', key, data2[key]))
+            data_for_diff.append(('-', key, data1[key]))
+            data_for_diff.append(('+', key, data2[key]))
         else:
-            data_diff.append(('-', key, data1[key]))
+            data_for_diff.append(('-', key, data1[key]))
     for key in data2:
         if key not in data1:
-            data_diff.append(('+', key, data2[key]))
+            data_for_diff.append(('+', key, data2[key]))
 
-    data_diff.sort(key=itemgetter(1))
+    data_for_diff.sort(key=itemgetter(1))
 
-    result_diff = "{\n"
-    for item in data_diff:
-        result_diff += '  {} {}: {}\n'.format(str(item[0]), str(item[1]), str(item[2]))
-    result_diff += "}"
-    return result_diff
+    diff = "{\n"
+    for item in data_for_diff:
+        diff += '  {} {}: {}\n'.format(str(item[0]), str(item[1]), str(item[2]))
+    diff += "}"
+    return diff
 
 
 def main():
@@ -72,7 +69,7 @@ def main():
     parser.add_argument('first_file', metavar='first_file', type=str, nargs=1,
                         help='First file to compare')
     parser.add_argument('second_file', metavar='second_file', type=str, nargs=1,
-                    help='Second file to compare')
+                        help='Second file to compare')
     parser.add_argument('-f', '--format', help='plain or JSON')
     args = parser.parse_args()
     print(args)
