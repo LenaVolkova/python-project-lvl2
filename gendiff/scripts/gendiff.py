70c004f4b394c6559e478ffa30c42f8fd68e3cd0
Elena Volkova <lenochka.volkova@gmail.com>
2021-10-21 10:04:26 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -56,21 +56,12 @@ def get_diff(dict1, dict2):
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-# parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                    help='an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                    const=sum, default=max,
-#                    help='sum the integers (default: find the max)')
-#
-# args = parser.parse_args()
-# print(args.accumulate(args.integers))
     parser.add_argument('first_file', metavar='first_file', type=str, nargs=1,
                         help='First file to compare')
     parser.add_argument('second_file', metavar='second_file', type=str, nargs=1,
                         help='Second file to compare')
     parser.add_argument('-f', '--format', help='plain or JSON')
     args = parser.parse_args()
-    # print(args)
     diff = generate_diff(args.first_file[0], args.second_file[0], args.format)
     print(diff)
 
